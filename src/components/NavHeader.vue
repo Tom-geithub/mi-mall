1bd17bd1fbd3b7bbcb39a42539ed98709d6cd3ab
TOM <mercifultom@aliyun.com>
2022-11-11 16:50:02 +0800

    5-2 header导航栏

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,66 +1,159 @@
 <template>
     <div class="header">
-      <div class="nav-topbar">
-        <div class="container">
-          <div class="topbar-menu">
-            <a href="javascript:;">小米商城</a>
-            <a href="javascript:;">MUI</a>
-            <a href="javascript:;">云服务</a>
-            <a href="javascript:;">协议规则</a>
-          </div>
-          <div class="topbar-user">
-            <a href="javascript:;">登录</a>
-            <a href="javascript:;">注册</a>
-            <a href="javascript:;" class="my-cart">
-              <span class="icon-cart"></span>
-              购物车
-            </a>
-          </div>
+        <!-- 顶部栏 -->
+        <div class="nav-topbar">
+            <div class="container">
+                <div class="topbar-menu">
+                    <a href="javascript:;">小米商城</a>
+                    <a href="javascript:;">MUI</a>
+                    <a href="javascript:;">云服务</a>
+                    <a href="javascript:;">协议规则</a>
+                </div>
+                <div class="topbar-user">
+                    <a href="javascript:;">登录</a>
+                    <a href="javascript:;">注册</a>
+                    <a href="javascript:;" class="my-cart">
+                        <span class="icon-cart"></span>
+                        购物车
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="nav-hearder"></div>
+        <!-- 导航栏 -->
+        <div class="nav-header">
+            <div class="container">
+                <div class="header-logo">
+                    <a href="/#/index"></a>
+                </div>
+                <div class="header-menu">
+                    <div class="item-menu">
+                        <span>小米手机</span>
+                        <div class="children"></div>
+                    </div>
+                    <div class="item-menu">
+                        <span>RedMi红米</span>
+                        <div class="children"></div>
+                    </div>
+                    <div class="item-menu">
+                        <span>电视</span>
+                        <div class="children"></div>
+                    </div>
+                </div>
+                <div class="header-search"></div>
+            </div>
+        </div>
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
+<script>
+export default {
     name: "nav-header",
-  };
-  </script> 
-  <style lang="scss">
-  .header {
+};
+</script> 
+<style lang="scss" scoped>
+@import './../assets/scss/base.scss';
+
+.header {
     .nav-topbar {
-      height: 39px;
-      line-height: 39px;
-      background-color: #333333;
-      color: #b0b0b0;
-      .container {
-        width: 1226px;
-        margin-right: auto;
-        margin-left: auto;
-        display: flex; //:设置元素两边对齐  中间居中
-        justify-content: space-between; //水平方向居中
-        align-items: center; //垂直方向居中
-        a {
-          display: inline-block;
-          color: #b0b0b0;
-          margin-right: 17px;
+        height: 39px;
+        line-height: 39px;
+        background-color: #333333;
+        color: #b0b0b0;
+
+        .container {
+            display: flex; //:设置元素两边对齐  中间居中
+            justify-content: space-between; //水平方向居中
+            align-items: center; //垂直方向居中
+
+            a {
+                display: inline-block;
+                color: #b0b0b0;
+                margin-right: 17px;
+            }
+
+            .my-cart {
+                width: 110px;
+                background-color: #ff6600;
+                text-align: center;
+                color: #ffffff;
+
+                .icon-cart {
+                    display: inline-block;
+                    width: 16px;
+                    height: 12px;
+                    margin-right: 4px;
+                    background: url("/public/imgs/icon-cart-checked.png") no-repeat center;
+                    background-size: contain;
+                }
+            }
         }
-        .my-cart {
-          width: 110px;
-          background-color: #ff6600;
-          text-align: center;
-          color: #ffffff;
-          .icon-cart {
-            display: inline-block;
-            width: 16px;
-            height: 12px;
-            margin-right: 4px   ;
-            background: url("/public/imgs/icon-cart-checked.png") no-repeat center;
-            background-size: contain;
-          }
-        }
-      }
     }
-  }
-  </style>
+
+    .nav-header {
+        .container {
+            height: 112px;
+            display: flex; //:设置元素两边对齐  中间居中
+            justify-content: space-between; //水平方向居中
+            align-items: center; //垂直方向居中
+
+            .header-logo {
+                display: inline-block;
+                width: 55px;
+                height: 55px;
+                background-color: #ff6600;
+
+                a {
+                    display: inline-block;
+                    width: 110px;
+                    height: 55px;
+
+                    &:before {
+                        content: ' ';
+                        display: inline-block;
+                        width: 55px;
+                        height: 55px;
+                        background: url('/public/imgs/mi-logo.png') no-repeat center;
+                        background-size: 55px;
+                        transition: margin .2s; //过渡动画
+                    }
+
+                    &:after {
+                        content: ' ';
+                        display: inline-block;
+                        width: 55px;
+                        height: 55px;
+                        background: url('/public/imgs/mi-home.png') no-repeat center;
+                        background-size: 55px;
+                    }
+
+                    //鼠标移上去图片变化
+                    &:hover:before {
+                        margin-left: -55px;
+                        transition: margin .2s; //过渡动画
+                    }
+                }
+            }
+
+            .header-menu{
+                display: inline-block;
+                width: 200px;
+                padding-left: 209px;
+                .item-menu{
+                    display: inline-block;
+                    color: #333333;
+                    font-weight: bold;
+                    font-size: 16px;
+                    line-height: 112px;
+                    span{
+                        cursor:pointer;//让鼠标移上去是小手标志
+                    }
+                    &:hover{
+                        
+                    }
+                }
+            }
+        }
+    }
+
+}
+</style>
