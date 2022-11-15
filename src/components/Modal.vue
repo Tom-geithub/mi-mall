219dc555a3bfcf6cc56cc299cfc38b6aeb85721c
tom <mercifultom@aliyun.com>
2022-11-15 18:38:53 +0800

    5-15 modal弹窗静态完成

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,8 +4,8 @@
         <div class="modal-dialog">
             <!-- 头部 -->
             <div class="modal-header">
-                <span></span>
-                <a href=""></a>
+                <span>提示</span>
+                <a href="javascript:;" class="icon-close"></a>
             </div>
             <!-- 内容 -->
             <div class="modal-body">
@@ -13,42 +13,44 @@
             </div>
             <!-- 尾部 -->
             <div class="modal-footer">
-                <a href="javascript:;" class="btn">确定</a>
-                <a href="javascript:;" class="btn">取消</a>
+                <div class="btn-group">
+                    <a href="javascript:;" class="btn">确定</a>
+                    <a href="javascript:;" class="btn btn-default">取消</a>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default{
-        name:'mall-modal',
-        props:{
-            // 弹框类型：小small，中middle，大large，表单form
-            modalType:{
-                type:String,
-                dafault:'form'
-            },
-            //弹框标题
-            title:String,
-            //按钮类型：1：确定类型 2：取消类型 3：确定取消
-            btnType:String,
-            sureText:{
-                type:String,
-                dafault:'确定'
-            },
-            canelText:{
-                type:String,
-                dafault:'取消'
-            },
-            showModal:Boolean
-        }
+export default {
+    name: 'mall-modal',
+    props: {
+        // 弹框类型：小small，中middle，大large，表单form
+        modalType: {
+            type: String,
+            dafault: 'form'
+        },
+        //弹框标题
+        title: String,
+        //按钮类型：1：确定类型 2：取消类型 3：确定取消
+        btnType: String,
+        sureText: {
+            type: String,
+            dafault: '确定'
+        },
+        canelText: {
+            type: String,
+            dafault: '取消'
+        },
+        showModal: Boolean
     }
+}
 </script>
 
 <style lang="scss" scoped>
 @import './../assets/scss/config.scss';
 @import './../assets/scss/mixin.scss';
 @import './../assets/scss/modal.scss';
-
+@import './../assets/scss/button.scss';
 </style>
