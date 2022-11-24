d28b9f71eda93e3495c4e41ee130e236c215d763
tom <mercifultom@aliyun.com>
2022-11-24 12:10:45 +0800

    订单页面搭建，商品数据和地址的获取

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -2,19 +2,45 @@
 
 <template>
     <div>
-        <OrderHeaderVue></OrderHeaderVue>
+        <OrderHeaderVue :title="title">
+            <template v-slot:tip>
+                <span>{{tip}}</span>
+            </template>
+        </OrderHeaderVue>
         <router-view></router-view>
+        <ServiceBarVue></ServiceBarVue>
         <NavFooterVue></NavFooterVue>
     </div>
 </template>
 <script>
 import NavFooterVue from '@/components/NavFooter.vue';
 import OrderHeaderVue from '@/components/OrderHeader.vue';
+import ServiceBarVue from '@/components/ServiceBar.vue';
 export default {
     name: 'nav-order',
-    components:{
+    components: {
         NavFooterVue,
-        OrderHeaderVue
+        OrderHeaderVue,
+        ServiceBarVue
+    },
+    data (){
+        return{
+            title:' ',
+            tip:' '
+        }
+    },
+    mounted (){
+        let path=this.$route.path;
+        if(path == '/order/confirm'){
+            this.title="订单确认";
+            this.tip='请认真确认订单信息';
+        }else if(path=='/order/list'){
+            this.title="订单列表";
+            this.tip='请谨防钓鱼连接或诈骗电话，了解更多>';
+        }else if(path=='/order/pay'){
+            this.title="订单支付";
+            this.tip='请谨防钓鱼连接或诈骗电话，了解更多>';
+        }
     }
 }
 </script>
